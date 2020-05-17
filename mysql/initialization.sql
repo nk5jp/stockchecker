7313f2f7540a5e611ba2033931af20bf32fe0811
nk5jp <keigo.nagata@gmail.com>
2020-05-17 21:05:11 +0900

    create sql for initialization.

---
+++ mysql/initialization.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS stockapp;
+USE stockapp;
+
+CREATE TABLE IF NOT EXISTS stock
+(
+    code SMALLINT UNSIGNED NOT NULL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS daily
+(
+    code SMALLINT UNSIGNED NOT NULL,
+    get_date MEDIUMINT UNSIGNED NOT NULL,
+    price DECIMAL(8,1) NOT NULL,
+    PRIMARY KEY(code, get_date),
+    FOREIGN KEY fk_d_code (code) REFERENCES stock (code) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS watch_by_code
+(
+    code SMALLINT UNSIGNED NOT NULL,
+    is_upper_bound BIT NOT NULL,
+    price DECIMAL(8,1) NOT NULL,
+    PRIMARY KEY(code, is_upper_bound),
+    FOREIGN KEY fk_wbc_code (code) REFERENCES stock (code) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS watch_by_trend
+(
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    duration SMALLINT UNSIGNED NOT NULL,
+    is_positive BIT NOT NULL
+);
