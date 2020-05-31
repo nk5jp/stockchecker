26bc87f078111a7e01101d3c3b31b0eb3a43dcdb
nk5jp <keigo.nagata@gmail.com>
2020-05-31 16:35:17 +0900

    make try and except parts.

--- python/getCurrentPrice.py
+++ python/getCurrentPrice.py
@@ -9,15 +9,20 @@ def main():
     stocks = dao.selectAllStock()
     watchByCode = dao.selectAllWatchByCode()
     for stock in stocks:
-        code = stock[0]
-        stockInfo = minkabuPerser.getStockInfo(code)
-        if code == stockInfo[0]:
-            dao.insertDaily(code, stockInfo[2], stockInfo[3])
-            if (code in watchByCode):
-                if (float(stockInfo[3]) < watchByCode[code][0]):
-                    print (f'{code}: {stockInfo[3]} is lower than {watchByCode[code][0]}')
-                if (float(stockInfo[3]) > watchByCode[code][1]):
-                    print (f'{code}: {stockInfo[3]} is upper than {watchByCode[code][1]}')
+        try:
+            code = stock[0]
+            stockInfo = minkabuPerser.getStockInfo(code)
+            if code == stockInfo[0]:
+                dao.insertDaily(code, stockInfo[2], stockInfo[3])
+                if (code in watchByCode):
+                    if (float(stockInfo[3]) < watchByCode[code][0]):
+                        print (f'{code}: {stockInfo[3]} is lower than {watchByCode[code][0]}')
+                    if (float(stockInfo[3]) > watchByCode[code][1]):
+                        print (f'{code}: {stockInfo[3]} is upper than {watchByCode[code][1]}')
+        except:
+            print(f'{code}: some error has been occered and skipped.')
+            continue
+    dt_now =  datetime.datetime.now()
     now = dt_now.strftime('%Y年%m月%d日 %H:%M:%S')
     print(f'{now}: getCurrentPrice end')
 
