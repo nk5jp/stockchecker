175d983083bfce0936b76f92a8eb4d58084346ad
nk5jp <keigo.nagata@gmail.com>
2020-06-28 15:13:21 +0900

    apply notification.

--- android/app/src/main/java/jp/nk5/stockchecker/MainActivity.kt
+++ android/app/src/main/java/jp/nk5/stockchecker/MainActivity.kt
@@ -2,11 +2,10 @@ package jp.nk5.stockchecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
+import android.widget.ListView
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
-import androidx.constraintlayout.widget.Constraints.TAG
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +16,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+
         FirebaseInstanceId.getInstance().instanceId
             .addOnCompleteListener(OnCompleteListener { task ->
                 if (!task.isSuccessful) {
-                    Log.w(TAG, "getInstanceId failed", task.exception)
                     return@OnCompleteListener
                 }
 
@@ -28,11 +27,21 @@ class MainActivity : AppCompatActivity() {
                 val token = task.result?.token
 
                 // Log and toast
-                Log.d(TAG, token)
                 val messageView: EditText = findViewById(R.id.editText)
                 messageView.setText(token)
-
             })
+
+        val dao = NotificationsDAO(this)
+
+        if (intent.getExtras() != null) {
+            val date = intent.getExtras().getString("date").toInt()
+            val message = intent.getExtras().getString("message")
+            dao.createNotification(MyNotification(-1, date, message))
+        }
+
+        val messageList = dao.getAll()
+        val listView: ListView = findViewById(R.id.listView)
+        listView.adapter = MessageListAdapter(this, android.R.layout.simple_list_item_1, messageList.take(10))
     }
 
 }
--- android/app/src/main/java/jp/nk5/stockchecker/MyFirebaseMessagingService.kt
+++ android/app/src/main/java/jp/nk5/stockchecker/MyFirebaseMessagingService.kt
@@ -3,6 +3,7 @@ package jp.nk5.stockchecker
 import com.google.firebase.messaging.FirebaseMessagingService
 import android.util.Log
 import androidx.constraintlayout.widget.Constraints.TAG
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
 
@@ -14,5 +15,15 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         // Instance ID token to your app server.
         //sendRegistrationToServer(token)
     }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        val date = p0.data["date"]!!.toInt()
+        val message = p0.data["message"]!!
+
+        val dao = NotificationsDAO(this)
+        dao.createNotification(MyNotification(-1, date, message))
+    }
+
 
 }
